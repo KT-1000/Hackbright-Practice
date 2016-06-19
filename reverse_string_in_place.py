943ec26903d0aa3b01397c81fd3129ba1223eb22
annihilatrix <katie@katie.codes>
2016-06-19 15:31:11 -0700

    Added doctests

--- reverse_string_in_place.py
+++ reverse_string_in_place.py
@@ -1,9 +1,14 @@
 def rev_string(s):
-    """ Reverse a string s in place without using any builtins, like reverse()"""
+    """ Reverse a string s in place without using any builtins, like reverse()
+    >>> rev_string('porcupine')
+    'enipucrop'
+    >>> rev_string("a")
+    'a'
+    >>> rev_string("racecar")
+    'racecar'
+    """
     new_string = ""
     for i in range(len(s), 0, -1):
         new_string += s[i - 1]
 
     return new_string
-
-print rev_string("porcupine")
